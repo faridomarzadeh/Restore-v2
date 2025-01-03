cb6856ba66523fcb67bad1eeffb1c59fe175fe68
Farid Omarzadeh <farid.omarzadeh29@gmail.com>
2025-01-02 22:29:44 -0500

    finished with react detail page

--- client/src/features/catalog/ProductDetails.tsx
+++ client/src/features/catalog/ProductDetails.tsx
@@ -1,22 +1,95 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom"
+import { useParams } from "react-router-dom";
 import { Product } from "../../app/models/product";
-
+import {
+    Button,
+  Divider,
+  Grid2,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableRow,
+  TextField,
+  Typography,
+} from "@mui/material";
 
 export default function ProductDetails() {
+  const { id } = useParams();
 
-    const {id} = useParams();
+  const [product, setProduct] = useState<Product | null>(null);
 
-    const [product, setProduct] = useState<Product | null>(null);
+  useEffect(() => {
+    fetch(`https://localhost:5001/api/products/${id}`)
+      .then((res) => res.json())
+      .then((data) => setProduct(data))
+      .catch((error) => console.log(error));
+  }, [id]);
 
-    useEffect(() => {
-        fetch(`https://localhost:5001/api/products/${id}`)
-        .then( res => res.json())
-        .then( data => setProduct(data))
-        .catch(error => console.log(error));
-    },[id])
+  if (!product) return <div>Loding...</div>;
+
+
+  const productDetails = [
+    {label: 'Name', value: product.name},
+    {label: 'Description', value: product.description},
+    {label: 'Type', value: product.type},
+    {label: 'Brand', value: product.brand},
+    {label: 'Quantity in stock', value: product.quantityInStock}
+  ]
 
   return (
-    <div>{product?.name}</div>
-  )
+    <Grid2 container spacing={6} maxWidth="lg" sx={{ mx: "auto" }}>
+      <Grid2 size={6}>
+        <img
+          src={product.pictureUrl}
+          alt={product.name}
+          style={{ width: "100%" }}
+        />
+      </Grid2>
+      <Grid2 size={6}>
+        <Typography variant="h3">{product.name}</Typography>
+        <Divider sx={{ mb: 2 }} />
+        <Typography variant="h4" color="secondary">
+          ${(product.price / 100).toFixed(2)}
+        </Typography>
+        <TableContainer>
+          <Table sx={{
+            '& td' : {fontSize: '1rem'}
+          }}>
+            <TableBody>
+                {productDetails.map((details,index)=> (
+                         <TableRow key={index}>
+                            <TableCell sx={{fontSize:'bold'}}>{details.label}</TableCell>
+                            <TableCell sx={{fontSize:'bold'}}>{details.value}</TableCell>
+                         </TableRow>
+                ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+
+        <Grid2 container spacing={2} mt={3}>
+            <Grid2 size={6}>
+                <TextField
+                variant="outlined"
+                type="number"
+                label="Quantity in basket"
+                fullWidth
+                defaultValue={1}
+                />
+            </Grid2>
+            <Grid2 size={6} >
+                <Button
+                color="primary"
+                size="large"
+                variant="contained"
+                fullWidth
+                sx={{height:'55px'}}
+                >
+                    Add to basket
+                </Button>
+            </Grid2>
+        </Grid2>
+      </Grid2>
+    </Grid2>
+  );
 }
