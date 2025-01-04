ace57a547b8f8a9e9f09c0544129149bbae5162f
Farid Omarzadeh <farid.omarzadeh29@gmail.com>
2025-01-03 19:57:07 -0500

    setup react toolkit

--- client/src/features/contact/ContactPage.tsx
+++ client/src/features/contact/ContactPage.tsx
@@ -1,6 +1,23 @@
+import { useDispatch } from "react-redux"
+import { decrement, increment } from "./counterReducer"
+import { Button, ButtonGroup, Typography } from "@mui/material";
+import { useAppSelector } from "../../app/store/store";
 
 export default function ContactPage() {
+
+    const data = useAppSelector(state => state.counter.data);
+    const dispatch = useDispatch();
   return (
-    <div>ContactPage</div>
+   <>
+   <Typography variant="h2">Contact Page</Typography>
+
+   <Typography variant="body1"> the data is : {data}</Typography>
+
+   <ButtonGroup>
+    <Button onClick={() => dispatch(decrement(1))} color="error">Decrement - </Button>
+    <Button onClick={() => dispatch(increment(1))} color="success">Increment + </Button>
+    <Button onClick={() => dispatch(increment(5))} color="success">Increment by 5 </Button>
+   </ButtonGroup>
+   </>
   )
 }
