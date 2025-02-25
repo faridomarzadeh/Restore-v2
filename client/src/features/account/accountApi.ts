199dcc9cb95571894164a51bf5f9caf097a199cf
Farid Omarzadeh <farid.omarzadeh29@gmail.com>
2025-02-25 09:18:52 -0500

    persisting address

--- client/src/features/account/accountApi.ts
+++ client/src/features/account/accountApi.ts
@@ -1,6 +1,6 @@
 import { createApi } from "@reduxjs/toolkit/query/react";
 import { baseQueryWithErrorHandling } from "../../app/api/baseApi";
-import { User } from "../../app/models/user";
+import { Address, User } from "../../app/models/user";
 import { LoginSchema } from "../../lib/schemas/loginSchema";
 import { router } from "../../routes/Routes";
 import { toast } from "react-toastify";
@@ -71,9 +71,35 @@ export const AccountApi = createApi({
                     
                 }
             }
+        }),
+        fetchAddress : builder.query<Address,void>({
+            query: () => 'account/address'
+        }),
+        updateUserAddress : builder.mutation<Address,Address>({
+            query: (address) => ({
+                url: 'account/address',
+                method: 'POST',
+                body: address
+            }),
+            onQueryStarted: async( address, {dispatch, queryFulfilled}) => {
+                const patchResult = dispatch(AccountApi.util.updateQueryData('fetchAddress', undefined, (draft) => {
+                    Object.assign(draft, {...address})
+                }))
+
+                try {
+                    await queryFulfilled;
+                } catch (error) {
+                    patchResult.undo();
+                    console.log(error);
+                    
+                }
+            }
         })
     })
 })
 
 
-export const {useLoginMutation, useRegisterMutation, useUserInfoQuery, useLogoutMutation, useLazyUserInfoQuery} = AccountApi;
+export const {useLoginMutation, useRegisterMutation,
+    useUserInfoQuery, useLogoutMutation,
+    useLazyUserInfoQuery, useFetchAddressQuery,
+    useUpdateUserAddressMutation} = AccountApi;
--- client/src/features/checkout/CheckoutStepper.tsx
+++ client/src/features/checkout/CheckoutStepper.tsx
@@ -1,54 +1,115 @@
-import { Box, Button, Checkbox, FormControlLabel, Paper, Step, StepLabel, Stepper } from "@mui/material";
-import { AddressElement, PaymentElement } from "@stripe/react-stripe-js";
-import { useState } from "react"
+import {
+  Box,
+  Button,
+  Checkbox,
+  FormControlLabel,
+  Paper,
+  Step,
+  StepLabel,
+  Stepper,
+  Typography,
+} from "@mui/material";
+import {
+  AddressElement,
+  PaymentElement,
+  useElements,
+} from "@stripe/react-stripe-js";
+import { useState } from "react";
 import Review from "./Review";
+import {
+  useFetchAddressQuery,
+  useUpdateUserAddressMutation,
+} from "../account/accountApi";
+import { Address } from "../../app/models/user";
 
-const steps = ['Address', 'Payment', 'Review'];
+const steps = ["Address", "Payment", "Review"];
 
 export default function CheckoutStepper() {
+  const [activeStep, setActiveStep] = useState(0);
 
-    const [activeStep, setActiveStep] = useState(0);
+  const { data: { name, ...restAddress } = {} as Address , isLoading } =
+    useFetchAddressQuery();
 
-    const handleNext = () => {
-        setActiveStep(step => step + 1);
+  const [addressChecked, setAddressChecked] = useState(false);
+  const [updateAddress] = useUpdateUserAddressMutation();
+  const elements = useElements();
+
+  const handleNext = async () => {
+    if (activeStep === 0 && addressChecked && elements) {
+      const address = await getStripeAddress();
+      if (address) await updateAddress(address);
     }
+    setActiveStep((step) => step + 1);
+  };
 
-    const handleBack = () => {
-        setActiveStep(step => step -1);
-    }
+  const getStripeAddress = async () => {
+    const addressElement = elements?.getElement("address");
+
+    if (!addressElement) return null;
+
+    const {
+      value: { name, address },
+    } = await addressElement.getValue();
+    if (name && address) return { ...address, name };
+
+    return null;
+  };
+
+  const handleBack = () => {
+    setActiveStep((step) => step - 1);
+  };
+
+  if(isLoading)
+    return <Typography>Loading Address...</Typography>
+
   return (
-    <Paper sx={{p: 3, borderRadius: 3}}>
-        <Stepper activeStep={activeStep}>
-            {steps.map((label,index) => {
-                return (
-                    <Step key={index}>
-                        <StepLabel>{label}</StepLabel>
-                    </Step>
-                )
-            })}
-        </Stepper>
-        <Box sx={{mt: 2}}>
-            <Box display={activeStep===0 ? 'block': 'none'}>
-                <AddressElement
-                options={{mode: 'shipping'}}
-                />
-                <FormControlLabel
-                sx={{display: 'flex', justifyContent: 'end'}}
-                control={<Checkbox/>}
-                label="save as default address"
-                />
-            </Box>
-            <Box display={activeStep===1 ? 'block': 'none'}>
-                <PaymentElement/>
-            </Box>
-            <Box display={activeStep===2 ? 'block': 'none'}>
-                <Review/>
-            </Box>
+    <Paper sx={{ p: 3, borderRadius: 3 }}>
+      <Stepper activeStep={activeStep}>
+        {steps.map((label, index) => {
+          return (
+            <Step key={index}>
+              <StepLabel>{label}</StepLabel>
+            </Step>
+          );
+        })}
+      </Stepper>
+      <Box sx={{ mt: 2 }}>
+        <Box display={activeStep === 0 ? "block" : "none"}>
+          <AddressElement
+            options={{
+              mode: "shipping",
+              defaultValues: {
+                name: name,
+                address: restAddress,
+              },
+            }}
+          />
+          <FormControlLabel
+            sx={{ display: "flex", justifyContent: "end" }}
+            control={
+              <Checkbox
+                checked={addressChecked}
+                onChange={(e) => setAddressChecked(e.target.checked)}
+              />
+            }
+            label="save as default address"
+          />
         </Box>
-        <Box display='flex' justifyContent='space-between' paddingTop={2}>
-            <Button onClick={handleBack} disabled={activeStep < 1}>Back</Button>
-            <Button onClick={handleNext} disabled={activeStep > 1}>Next</Button>
+        <Box display={activeStep === 1 ? "block" : "none"}>
+          <PaymentElement />
         </Box>
+        <Box display={activeStep === 2 ? "block" : "none"}>
+          <Review />
+        </Box>
+      </Box>
+      <Box display="flex" justifyContent="space-between" paddingTop={2}>
+        <Button onClick={handleBack} disabled={activeStep < 1}>
+          Back
+        </Button>
+        <Button onClick={handleNext} disabled={activeStep > 1}>
+          Next
+        </Button>
+      </Box>
     </Paper>
-  )
+  );
 }
